ccc8f2b955112b57544b4b0ed04a21e49088cf79
Ashish <ashish.rai@digivive.com>
2024-01-26 19:49:28 +0530

    frontend setup and tailwind setup

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>This is home page
+    
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    
+    </div>
+    
+  )
+}
+
+export default Home
